21e52a26cf251a20c8c5e1ec1619fa04657a4ef8
IbrahimPatrawala <83429262+ibrahimpatra@users.noreply.github.com>
2026-04-17 11:29:19 +0000

    Remove static export, migrate to Vercel

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 🔥 THIS is the missing piece
-
   reactStrictMode: true,
-
   images: {
-    unoptimized: true, // required for static export
     domains: [
       'customer-assets.emergentagent.com',
       'images.unsplash.com',
       'images.pexels.com',
       'i.ibb.co',
-      'www.google.com'
     ],
   },
 };
